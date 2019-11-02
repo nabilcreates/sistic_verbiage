3c1f5b3f6001e6c162065d5b18a2cc3146280b78
Nabil Ridhwanshah <nabilrdhwan@gmail.com>
2019-11-02 09:41:07 +0800

    Update

---
+++ consts.js
@@ -0,0 +1,12 @@
+const select_values = ["AA", "VC", "E-ticket", "Mail", "Courier"]
+const text_ending = [
+    `Your tickets are immediately available for collection and will remain so up to the event date. Kindly produce the same credit card used for this purchase and either print out or show the email confirmation page upon collection for verification purposes`,
+
+    `You may collect your tickets at the venue from an hour before the showtime. Kindly produce the same credit card used for this purchase and either print out or show the email confirmation page upon collection for verification purposes upon collection`,
+
+    `Your booking confirmation will include instructions to download your E-tickets. Remember to print them out or just show it on your phone as you will need to present them during admission.`,
+
+    `Kindly allow up to 7 business days for the delivery to be fulfilled. If tickets are not received by then, simply give us a call to look into the matter`,
+
+    `Someone from my office will be contacting you by phone within 1 business day to confirm the ticket delivery arrangement`,
+]
